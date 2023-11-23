7f6239293825948078f9373d2c30360337796833
apally <adrien.pally@orange.com>
2023-11-23 13:52:56 +0100

    basic provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-void main() {
-  runApp(const MainApp());
+part 'main.g.dart';
+
+@riverpod
+String helloWorld(Ref ref) {
+  return 'HelloWold';
 }
 
-class MainApp extends StatelessWidget {
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
+}
+
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final text = ref.watch(helloWorldProvider);
+
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(text),
         ),
       ),
     );
